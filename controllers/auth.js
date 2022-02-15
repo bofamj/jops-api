28c7052ac751fd3753f2f4de5d8eb8d82ba46b0a
mustafa <mustafamjaber@yahoo.com>
2022-02-15 14:58:35 +0200

    added the login and the regester api methode

--- controllers/auth.js
+++ controllers/auth.js
@@ -44,15 +44,33 @@ module.exports = {
 
 const {StatusCodes} = require('http-status-codes')
 const {BadRequestError,UnauthenticatedError}= require('../errors')
+const User = require('../models/User')
+const jwt = require('jsonwebtoken')
 
-
-
-const register = (req,res)=>{
-    res.status(StatusCodes.CREATED).json({maseg:'uoser register'})
+const register = async (req,res)=>{
+    const {name, email, password}= req.body;
+    if(!email || !password ||!name ){
+        throw new BadRequestError('Please provide name email and password')
+    }
+    const user  = await User.create({...req.body})
+    const token = user.createJWT()
+    res.status(StatusCodes.CREATED).json({user:{name:user.name},token})
 }
-const login = (req,res)=>{
-    res.status(StatusCodes.OK).json({maseg:'uoser loged in'})
-
+const login = async (req,res)=>{
+    const {email, password}= req.body;
+    if(!email || !password ){
+        throw new BadRequestError('Please provide email and password')
+    }
+    const user  = await User.findOne({email})
+    if(!user){
+        throw new UnauthenticatedError('Invalid Credentials')
+    }
+    isPassword = await user.comparePassword(password)
+    if(!isPassword){
+        throw new UnauthenticatedError('Invalid Credentials')
+    }
+    const token = user.createJWT()
+    res.status(StatusCodes.OK).json({user:{name:user.name},token})
 }
 
 
--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+/* const mongoose = require('mongoose')
 
 
 const bcrypt = require('bcryptjs')
@@ -42,4 +42,52 @@ UserSchema.methods.comparePassword = async function (canditatePassword){
 }
 
 
-module.exports = mongoose.model('User',UserSchema)
+module.exports = mongoose.model('User',UserSchema) */
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const UserSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,'please provide name'],
+        minlength:3,
+        maxlength:50
+    },
+    email:{
+        type:String,
+        required:[true,'please provide email'],
+        match:[/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,'please provide a valid email'],
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:[true,'please provide password'],
+        minlength:6,
+    },
+})
+
+//* crbting password
+
+UserSchema.pre('save', async function (next){
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
+})
+
+
+//*creating token 
+
+UserSchema.methods.createJWT = function (){
+    return jwt.sign({userID:this._id, name:this.name},process.env.JWT_SECRET,{
+        expiresIn:process.env.JWT_LIFETIME
+    })
+}
+
+//* compare password
+
+UserSchema.methods.comparePassword = async function (anditatePassword){
+    const isMatch = await bcrypt.compare(anditatePassword,this.password)
+    return isMatch
+}
+
+module.exports = mongoose.model('User',UserSchema);
